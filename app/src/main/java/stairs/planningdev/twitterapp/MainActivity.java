71e70c8fd45ab1e1129580630a05f4ea00986a8d
Kikuda Naoto <abakankncts701@gmail.com>
2017-06-26 23:38:00 +0900

    ツイートの単純なListView表示まで

--- app/src/main/java/stairs/planningdev/twitterapp/MainActivity.java
+++ app/src/main/java/stairs/planningdev/twitterapp/MainActivity.java
@@ -4,15 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
     // タイムラインを表示するリストビュー
     private ListView mTLList;
-    //ツイート入力画面へ遷移するボタン
+    // ツイート内容を保持するためのアダプター
+    private ArrayAdapter<String> mTLAdapter;
+    private ArrayList<String> mTweetList;
+    // ツイート入力画面へ遷移するボタン
     private ImageButton mTweetButton;
     private TextView mTestText;
 
@@ -21,9 +26,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTLList = (ListView) findViewById(R.id.timeline);
         mTweetButton = (ImageButton) findViewById(R.id.tweet);
         mTestText = (TextView) findViewById(R.id.text_test);
+        mTLList = (ListView) findViewById(R.id.timeline);
+
+        mTLAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        mTLList.setAdapter(mTLAdapter);
 
         mTweetButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,14 +42,23 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //startActivityForResult で起動したアクティビティが finish() で破棄されたとき呼び出される
     @Override
-    protected void onActivityResult(int requestCode,int resultCode, Intent intent){
+    protected void onActivityResult(int requestCode,int resultCode, Intent intent) {
         String strTweet;
-        super.onActivityResult(requestCode,resultCode,intent);
-
+        super.onActivityResult(requestCode, resultCode, intent);
+        //RESULT_OKが返ってきたらintentから値取得
+        /*if (resultCode == RESULT_OK) {
+            //intentから返ってきた値取得
+            strTweet = intent.getStringExtra("TWEET_RESULT");
+            mTestText.setText(strTweet);
+        }
+    }*/
         if(resultCode == RESULT_OK) {
             strTweet = intent.getStringExtra("TWEET_RESULT");
             mTestText.setText(strTweet);
+            mTLAdapter.add(strTweet);
+            mTLAdapter.notifyDataSetChanged();
         }
     }
 }
--- app/src/main/java/stairs/planningdev/twitterapp/TweetActivity.java
+++ app/src/main/java/stairs/planningdev/twitterapp/TweetActivity.java
@@ -8,25 +8,29 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class TweetActivity extends AppCompatActivity {
-    private EditText mTweetEdit;
-    private Button mTweetButton;
 
-    private String mTweetText;
+    //ツイート文入力用のEditText宣言
+    private EditText mTweetEdit;
+    //ツイートボタン宣言
+    private Button mTweetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweet);
 
+        //いつも通りidと紐付け
         mTweetEdit = (EditText) findViewById(R.id.tweet_edit);
         mTweetButton = (Button) findViewById(R.id.send_button);
 
+        //ツイートボタンをクリックしたときの処理
         mTweetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mTweetText = mTweetEdit.getText().toString();
                 Intent intent = getIntent();
-                intent.putExtra("TWEET_RESULT",mTweetText);
+                //intentに元のアクティビティへ返す値を渡しておく
+                intent.putExtra("TWEET_RESULT",mTweetEdit.getText().toString());
+                //元のアクティビティに結果を返す
                 setResult(RESULT_OK,intent);
                 finish();
             }
